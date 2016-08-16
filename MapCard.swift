c997ae4222bb0583683062a66dda1c1fd72e01a3
Christopher Overton <christopheroverton@christohersmini>
2016-08-16 18:10:24 -0500

    save

---
+++ MapCard.swift
@@ -0,0 +1,16 @@
+//
+//  MapCard.swift
+//  ChannelIsland
+//
+//  Created by Christopher Overton on 8/16/16.
+//  Copyright © 2016 Christopher Overton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct MapCard {
+    let name: String
+    let description: String
+    let image: UIImage
+}
