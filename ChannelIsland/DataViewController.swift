6e102a7c92c8fc27c9b06127e52af6868fcc2960
Christopher Overton <christopheroverton@christohersmini>
2016-08-18 00:36:50 -0500

    save

--- ChannelIsland/DataViewController.swift
+++ ChannelIsland/DataViewController.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var TextView: UITextView!
+    @IBOutlet weak var IView2: UIImageView!
+    @IBOutlet weak var InfoButton: UIButton!
+    @IBOutlet weak var SantaBarbaraButton: UIButton!
+    @IBOutlet weak var AnacapaButton: UIButton!
+    @IBOutlet weak var SantaCruzButton: UIButton!
+    @IBOutlet weak var SantaRosaButton: UIButton!
+    @IBOutlet weak var SanMigButton: UIButton!
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var IView: UIImageView!
+    @IBOutlet var MView: UIView!
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
-
+    //var lightBlur: UIBlurEffect
+    //var blurView: UIVisualEffectView
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.ScrollView.contentSize.height = 1000
+        self.ScrollView.hidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +45,19 @@ class DataViewController: UIViewController {
     }
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    @IBAction func infoClick(sender: AnyObject) {
+        self.ScrollView.hidden = false
+        UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
+            self.IView.alpha = 0
+            self.ScrollView.alpha = 1
+            self.IView2.alpha = 1
+            }, completion: { finished in
+                if (finished){
+                    self.IView.hidden = true
+                }
+        })
+        print("Info Button Pressed")
     }
 }
 
