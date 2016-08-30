e41d49634fa10aac123cb61ffd4c4538b36ba194
Christopher Overton <christopheroverton@christohersmini>
2016-08-30 17:04:36 -0500

    save

---
+++ ChannelIsland/AnacapaViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AnacapaViewController.swift
+//  ChannelIsland
+//
+//  Created by Christopher Overton on 8/30/16.
+//  Copyright © 2016 Christopher Overton. All rights reserved.
+//
+
+
+import UIKit
+class AnacapaViewController: UIViewController, UITextViewDelegate {
+    
+    private let revealSequeId = "revealSegue"
+    private var swipeState: Bool = true  //right state
+    private let horizontalTransitionController = HorizontalTransitionController()
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print("Got This Far!")
+        print(segue.identifier)
+        var checkString: String = (segue.identifier)!
+        print(checkString.rangeOfString(revealSequeId) != nil)
+        if checkString.rangeOfString("Segue") != nil{
+            self.horizontalTransitionController.swipeDirection = false
+        }
+        else{
+            print("hit Seque")
+            self.horizontalTransitionController.swipeDirection = true
+        }
+        if checkString.rangeOfString("reveal") != nil, let destinationViewController = segue.destinationViewController as? UIViewController {
+            print("Got this far again")
+            destinationViewController.transitioningDelegate = self
+            print("Segue2 used!")
+            
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //self.dataLabel!.text = dataObject
+    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
+extension AnacapaViewController: UIViewControllerTransitioningDelegate {
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        //horizontalTransitionController.originFrame = CardView.frame
+        //horizontalTransitionController.swipeDirection = self.swipeState
+        return horizontalTransitionController
+    }
+}
