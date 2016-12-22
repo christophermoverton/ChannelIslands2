df79471289fed910365733b7473aeb7e7aecba75
Christopher Overton <christophermoverton@gmail.com>
2016-12-22 10:45:07 -0600

    Add Santa Cruz activities page model data

--- ChannelIsland/SantaCruzViewController.swift
+++ ChannelIsland/SantaCruzViewController.swift
@@ -44,6 +44,8 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
     @IBOutlet weak var PageIV6: UIImageView!
     private var pageIVs: [UIImageView!] = []
     private var capPagTVs: [UITextView!] = []
+    private var actpageIVs: [UIImageView!] = []
+    private var actpageTVs: [UITextView!] = []
     @IBOutlet weak var CapPagTV1: UITextView!
     @IBOutlet weak var CapPagTV2: UITextView!
     @IBOutlet weak var CapPagTV3: UITextView!
@@ -51,6 +53,48 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
     @IBOutlet weak var CapPagTV5: UITextView!
     @IBOutlet weak var CapPagTV6: UITextView!
     @IBOutlet weak var IPageV1: UIImageView!
+    private var Activitiestextstrings: [String] = ["ACTIVITIES\n\n","Boating and Kayaking\n\n","With one of the world’s largest sea caves and clear coastal waters, Santa Cruz is a sea kayaker’s paradise. Formal guided tours are offered at both Scorpion Bay and Prisoners Harbor.\n\n","Diving, Snorkeling, and Swimming\n\n","The easiest place for walk-in diving, snorkeling, and swimming is right off the pier at Scorpion Beach. To the east, Smuggler’s Cove offers great diving and snorkeling too. With the exception of Anacapa, these are the warmest waters you’re likely to find in the Channel Islands.\n\n","Wildlife Watching\n\n","With 145 species of life found nowhere else on Earth, the wildlife watcher is in for a treat on Santa Cruz.\n\nSanta Cruz Island is home to the island scrub jay and small island fox. They reveal two evolutionary strategies to adapt to the island’s unique ecosystem. The jay is bigger than its mainland cousins; the fox is much smaller.\n\n","Fishing\n\n","\t·Over 80 percent of the waters near the Channel Islands are open to fishing.\n\t·Sport fishing is allowed outside marine protected areas and requires possession of a valid California state fishing license with an ocean enhancement stamp.\n\t·All California Department of Fish and Game regulations apply.\n\n"," Hiking\n\n","\t·Several roads and trails traverse eastern Santa Cruz Island. Trails near historic Scorpion Ranch are well maintained and of moderate difficulty. Hiking trails in the more rugged Montañon area are generally more strenuous.\n\t·Rangers and naturalists offer guided tours year-round at Scorpion Anchorage and Prisoners Harbor.\n\t·Note that the western side of Santa Cruz requires a permit from the Nature Conservancy to enter.  For more information, please visit: https://www.nps.gov/chis/index.htm.\n\n","Camping\n\n","\t·Of all the five islands, Santa Cruz provides the most accommodations and amenities for campers. Take your pick between developed and well shaded campgrounds at Scorpion Bay and backcountry sites at Del Norte..\n\t·Year-round camping is available; overnight fees apply. Reserve your site well in advance at recreation.gov or call 877-444-6777.  Concession boats fill to capacity more quickly than campground sites are filled, so book your boat transportation for overnight trips first.\n\t·Scorpion Bay campground provides picnic tables, lock boxes for food, drinking water, and pit toilets.\n\t·Remember that you’ll be hauling everything else from the pier to your site, so bring essentials only.\n\t·Be prepared to pack in your own water when utilizing the backcountry campsites."]
+    
+    private var ActivitiesAttributes : [[String: AnyObject]] = [[NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSBackgroundColorAttributeName: UIColor.clearColor(),
+        NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSBackgroundColorAttributeName: UIColor.clearColor(),
+            NSFontAttributeName: UIFont(name: "Helvetica-Bold", size: 24.0)!],
+        [NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSBackgroundColorAttributeName: UIColor.clearColor(),
+            NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!]]
+    
     private var Infotextstrings: [String] = ["INFO\n\n","With relatively easy boat access, amazing kayaking, and great kelp beds near popular campsites, Santa Cruz offers an all-in-one destination. Located in the transition zone between warm southern currents and colder northern currents, Santa Cruz supports diverse marine life. Fishing is permitted outside reserves and protected areas. Land management is shared by the National Park Service and Nature Conservancy. Going ashore on the Nature Conservancy property requires a permit."]
     
     private var InfoAttributes : [[String: AnyObject]] = [[NSForegroundColorAttributeName: UIColor.whiteColor(),
@@ -90,10 +134,14 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
         [NSForegroundColorAttributeName: UIColor.whiteColor(),
             NSBackgroundColorAttributeName: UIColor.clearColor(),
             NSFontAttributeName: UIFont(name: "Helvetica-Light", size: 24.0)!]]
+    
+    
     private let photonames: [String] = ["PRISONERS_HARBOR","SCORPION_BAY_PIER","SCORPION_BAY_ANCHORAGES","AERIAL_SMUG","PELICAN_BAY_BOATERS","AERIAL_PELICAN"]
     private let infophotonames: [String] = ["SANTA_CRUZ_LOOKING_NORTH","SCORPION_CANYON_SC","SC_KAYAKERS"]
+    private let activitiesphotonames: [String] = ["PAINTED_CAVE_SCI","PAINTED_CAVE_SCI_2","SNORKLER_FREE_DIVING","POTATO_HARBOR","DIVER_HORN_SHARK","DIVER_KELP","WHALE_SIGHTING","PACIFIC_WHITE_SIDING","LOOK_FOR_WHALES","CALIFORNIA_SEA_LION","CSL_AT_PLAY","CLOSER_TO_SHORE","ISLAND_FOX_UNIQUE","ISLAND_SCRUB_JAY","LIVE_FOREVER","HOFFMANNS_SLENDER","HIKING_TO_POTATO_HILL","SC_NEAR_SBA"]
     private var InfoCaptiontextstrings: [String] = ["Santa Cruz Island, looking west. (photo by Tim Hauf)","Scorpion Canyon near Scorpion Bay anchorages, Santa Cruz Island","Kayakers at Potato Harbor, Santa Cruz Island"]
     private var AnchoragesCaptiontextstrings: [String] = ["Prisoner’s Harbor Pier, Santa Cruz Island","Scorpion Bay pier on Santa Cruz Island","Scorpion Bay anchorages, Santa Cruz Island","Aerial of anchorage at Smugglers’ Cove, Santa Cruz Island","Boaters at anchor in Pelican Bay, Santa Cruz Island","Aerial view of Pelican Bay, Santa Cruz Island"]
+    private var ActivitiesCaptiontextstrings: [String] = ["Painted Cave, Santa Cruz Island","Painted Cave, Santa Cruz Island","Snorkeler free-diving , Santa Cruz Island","Potato Harbor, Santa Cruz Island","Diver with horn shark.","Diving in the kelp, Santa Cruz Island","Whale sighting, Santa Barbara Channel (NPS)","Pacific White-sided Dolphins leaping","Look for whales, dolphins, and seabirds during your channel crossing.","California Sea Lion","California Sea Lions at play.","Closer to shore, you’re likely to see seals, sea lions, shorebirds, and bald eagles.","Island Fox, unique to the Channel Islands, photo by Tim Coonan","Island Scrub Jay","Santa Cruz Island live-forever, Santa Cruz Island","Hoffmann’s slender-flowered gilia, Santa Cruz Island","Santa Cruz Island, hiking to Potato Harbor ","Scorpion Canyon near Scorpion Bay anchorages, Santa Cruz Island"]
     private var InfoCaptionAttributes : [[String: AnyObject]] =
         [[NSForegroundColorAttributeName: UIColor.whiteColor(),
             NSBackgroundColorAttributeName: UIColor.clearColor(),
@@ -170,8 +218,44 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
             self.horizontalTransitionController.vname2 = destinationViewController.vname
         }
     }
+    
+    func loadActIVs(){
+        var y = 90.0
+        let x = 710.0
+        var i = 0
+        for astr: String in self.activitiesphotonames{
+            let ycg = CGFloat(y)
+            let xcg = CGFloat(x)
+            let ui = UIImage(imageLiteral: astr)
+            let imageView = UIImageView(image: ui)
+            let newheight = ui.size.height/1.85
+            let newwidth = ui.size.width/1.85
+            imageView.contentMode = .ScaleAspectFit
+            imageView.frame = CGRectMake(xcg, ycg, newwidth, newheight)//(piv.frame.x,piv.frame.y,piv.frame.width,newheight)
+            imageView.alpha = 0
+            
+            self.actpageIVs.append(imageView)
+            self.PageScrollView.addSubview(imageView)
+            let textView = UITextView()
+            let acstr: String = self.ActivitiesCaptiontextstrings[i]
+            let myAttributes = self.InfoCaptionAttributes[0]
+            let myAttrString1 = NSAttributedString(string: acstr,
+                                                   attributes: myAttributes)
+            textView.attributedText = myAttrString1
+            textView.frame = CGRectMake(xcg, ycg+newheight+10.0, 300.0, 60.0)
+            textView.alpha = 0
+            textView.backgroundColor = UIColor.clearColor()
+            self.actpageTVs.append(textView)
+            self.PageScrollView.addSubview(textView)
+            y = y + 60.0 + Double(newheight)
+            i+=1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadActIVs()
+        print(self.Activitiestextstrings.count)
         self.PageScrollView.delegate = self
         self.pageIVs = [self.PageIV1, self.PageIV2, self.PageIV3, self.PageIV4, self.PageIV5, self.PageIV6]
         self.capPagTVs = [self.CapPagTV1, self.CapPagTV2, self.CapPagTV3, self.CapPagTV4, self.CapPagTV5, self.CapPagTV6]
@@ -279,6 +363,7 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
         self.IView2.hidden = false
         self.PageScrollView.hidden = false
         if self.anchorageActive{
+            self.PageScrollView.contentSize.height = 1900
             var i = 0
             let result = NSMutableAttributedString()
             for astr: String in self.Anchoragestextstrings{
@@ -309,6 +394,7 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
         }
         
         if self.infoActive{
+            self.PageScrollView.contentSize.height = 1900
             var i = 0
             let result = NSMutableAttributedString()
             for astr: String in self.Infotextstrings{
@@ -320,6 +406,7 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
             }
             i = 0
             for piv: UIImageView in self.pageIVs{
+                piv.alpha = 1
                 if (i > 2){
                     piv.alpha = 0
                     self.capPagTVs[i].alpha = 0
@@ -346,6 +433,39 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
             }
             self.PageTextView.attributedText = result
         }
+        
+        if self.activitiesActive{
+            self.PageScrollView.contentSize.height = 4000
+            var i = 0
+            let result = NSMutableAttributedString()
+            for astr: String in self.Activitiestextstrings{
+                let myAttributes = self.ActivitiesAttributes[i]
+                let myAttrString1 = NSAttributedString(string: astr,
+                                                       attributes: myAttributes)
+                result.appendAttributedString(myAttrString1)
+                i+=1
+            }
+            i = 0
+            
+             for piv: UIImageView in self.pageIVs{
+
+             
+
+             //piv.frame = CGRectMake(piv.frame.origin.x, piv.frame.origin.y, piv.frame.size.width, newheight)//(piv.frame.x,piv.frame.y,piv.frame.width,newheight)
+             piv.alpha = 0
+             self.capPagTVs[i].alpha = 0
+
+             i+=1
+             }
+             i = 0
+            for piv: UIImageView! in self.actpageIVs{
+                piv.alpha = 1
+                self.actpageTVs[i].alpha = 1
+                i+=1
+            }
+            self.PageTextView.attributedText = result
+        }
+        
         UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
             self.IView.alpha = 0
             self.IView2.alpha = 1
@@ -385,6 +505,13 @@ class SantaCruzViewController: UIViewController, UITextViewDelegate, UIScrollVie
                     self.infoActive = false
                     self.anchorageActive = false
                     self.activitiesActive = false
+                    
+                    var i = 0
+                    for piv: UIImageView! in self.actpageIVs{
+                        piv.alpha = 0
+                        self.actpageTVs[i].alpha = 0
+                        i+=1
+                    }
                     
                 }
         })
